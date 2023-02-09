aad2bad58acc4a0be40d7098cef7048a985543ba
Nicolas Epstein <nicolas.epstein@qc.croixbleue.ca>
2023-02-09 11:01:41 -0500

    fix header field names to match frontend

--- devops_console/api/v2/dependencies.py
+++ devops_console/api/v2/dependencies.py
@@ -6,17 +6,17 @@ from devops_sccs.typing.credentials import Credentials
 class CommonHeaders:
     def __init__(
             self,
-            plugin_id: str = Header(default='cbq'),
-            username: str | None = Header(default=None),
-            apikey: str | None = Header(default=None),
-            author: str | None = Header(default=None),
+            x_plugin_id: str = Header(default='cbq'),
+            x_username: str | None = Header(default=None),
+            x_apikey: str | None = Header(default=None),
+            x_author: str | None = Header(default=None),
             ):
-        self.plugin_id = plugin_id
-        if username is None or author is None or apikey is None:
+        self.plugin_id = x_plugin_id
+        if x_username is None or x_author is None or x_apikey is None:
             self.credentials = None
         else:
             self.credentials = Credentials(
-                user=username,
-                apikey=apikey,
-                author=author,
+                user=x_username,
+                apikey=x_apikey,
+                author=x_author,
                 )
